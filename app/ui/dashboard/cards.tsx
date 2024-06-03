bfbaaab0998c6874063da0dde9d44e9486eab8f5
Luis Emmanuel <96873849+LuisEmmanuel27@users.noreply.github.com>
2024-06-03 17:11:19 -0600

    Finalizando el tema de Streaming

--- app/ui/dashboard/cards.tsx
+++ app/ui/dashboard/cards.tsx
@@ -5,6 +5,7 @@ import {
   InboxIcon,
 } from '@heroicons/react/24/outline';
 import { lusitana } from '@/app/ui/fonts';
+import { fetchCardData } from '@/app/lib/data';
 
 const iconMap = {
   collected: BanknotesIcon,
@@ -13,19 +14,14 @@ const iconMap = {
   invoices: InboxIcon,
 };
 
-type CardData = {
-  totalPaidInvoices: number;
-  totalPendingInvoices: number;
-  numberOfInvoices: number;
-  numberOfCustomers: number;
-};
+export default async function CardWrapper() {
+  const {
+    numberOfInvoices,
+    numberOfCustomers,
+    totalPaidInvoices,
+    totalPendingInvoices,
+  } = await fetchCardData();
 
-export default async function CardWrapper({
-  totalPaidInvoices,
-  totalPendingInvoices,
-  numberOfInvoices,
-  numberOfCustomers,
-}: CardData) {
   return (
     <>
       {/* NOTE: comment in this code when you get to this point in the course */}
